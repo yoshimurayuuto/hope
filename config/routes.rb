595f3309d0028c70ca1348d47ce9c10a357bc287
Yuuto Yoshimura <0615yutoyoshimu@gmail.com>
2019-11-27 17:58:53 +0900

    3回目のデプロイ

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # この記述を変更
   root :to => 'oauth_test#index'
   devise_for :users, controllers: {
+    confirmations: 'devise/confirmations',
     registrations: "users/registrations",
     # この記述を追記
     omniauth_callbacks: "users/omniauth_callbacks"
