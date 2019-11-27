c70250bc6ede3b5c4274f089cae21e3fcff653eb
Yuuto Yoshimura <0615yutoyoshimu@gmail.com>
2019-11-27 17:40:35 +0900

    ッサイデプロイ

--- db/schema.rb
+++ db/schema.rb
@@ -91,14 +91,15 @@ ActiveRecord::Schema.define(version: 2019_11_27_064021) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
